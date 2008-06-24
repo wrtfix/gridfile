ec13628837ecfc43a952e4b1f7a7dfdd512f630f
manuel.alonso.d <manuel.alonso.d@4028fbad-734f-0410-9096-773eaa3a677b>
2008-06-24 06:09:49 +0000

    agregando struct regMedicamento

--- trunk/src/main.cpp
+++ trunk/src/main.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
     
-void copiar(char a[100];char b[100]){
+void copiar(char a[100],char b[100]){
 	for (int i=0;i<100;i++)
 	{
 		b[i] = a[i];
@@ -15,13 +15,15 @@ void copiar(char a[100];char b[100]){
 
 int main(int argc, char *argv[])
 {
+	//Abrimos el archivo
     ifstream f("./datos_medicamentos.dat");
-    if(!f)
+    if (!f)
         cout << "fallo\n";
 
     /* Declaramos un array con suficiente tamaño para leer las líneas */
     char cadena[256];
-      /* Leemos */
+    
+    /* Leemos */
     char a[100];
 
     /* Leemos la siguiente línea */
@@ -34,7 +36,6 @@ int main(int argc, char *argv[])
         a[i]= cadena[i];
         i++;
     }
-    
     
     int nro_registro;
     nro_registro = atoi(a);
@@ -50,8 +51,6 @@ int main(int argc, char *argv[])
         j++;
     }
     
-    d = a;
-
     j = 0;
     i++;
     while(cadena[i] !=';')
@@ -64,18 +63,36 @@ int main(int argc, char *argv[])
     char *g = new char;
     g = a;
 
-
-
-    
-    cout << d <<endl;
-    
-    
-    
-    
-    
-    f.close();
-    
+    f.close();    
     
     system("pause");
     return EXIT_SUCCESS;
+
+	struct regMedicamento
+	{
+		//una secuencia de hasta 8 caracteres numéricos. (0 a 99999999)
+		int nro_registro;
+
+		//puede contener un máximo de 256 caracteres (letras y números).
+		char descripcion[100];
+
+		// puede contener un máximo de 16 caracteres (letras y números).
+		char laboratorio[16];
+
+		//una secuencia de hasta 4 caracteres numéricos. (0 a 9999)
+		short int accion_medicamento;
+
+		//una secuencia de hasta 2 caracteres numéricos. (0 a 99)
+		short int forma_medicamento;
+
+		//una secuencia de hasta 2 caracteres numéricos. (0 a 99)
+		short int tamanio_medicamento;
+
+		//una secuencia de hasta 2 caracteres numéricos. (0 a 99)
+		short int via_administracion;
+
+		//una secuencia de hasta 3 caracteres numéricos, una coma decimal y 2 caracteres numéricos más.
+		float precio;
+	};
+    
 }
