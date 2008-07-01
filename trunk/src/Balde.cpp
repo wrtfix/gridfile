6a638cab5366387c3f868e08beb967bc11e9cda5
manuel.alonso.d <manuel.alonso.d@4028fbad-734f-0410-9096-773eaa3a677b>
2008-07-01 00:22:16 +0000

    subo clases, todavia no las probé... besos

---
+++ trunk/src/Balde.cpp
@@ -0,0 +1,24 @@
+#include "Balde.h"
+#include <iostream.h>
+
+using namespace std;
+
+Balde::Balde() {
+
+	// TODO Auto-generated constructor stub
+}
+
+Balde::~Balde() {
+	// TODO Auto-generated destructor stub
+}
+
+bool Balde::add(int a) {
+	//if (elementos.size())
+	elementos.push_back(a);
+}
+
+int Balde::get(int i) {
+	return 3;
+}
+
+
---
+++ trunk/src/Gridfile.h
@@ -0,0 +1,10 @@
+#ifndef GRIDFILE_H_
+#define GRIDFILE_H_
+
+class Gridfile {
+public:
+	Gridfile();
+	virtual ~Gridfile();
+};
+
+#endif /* GRIDFILE_H_ */
