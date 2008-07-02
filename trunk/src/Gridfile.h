2f0624c38525294c0be46cffce9c01daf4099f21
manuel.alonso.d <manuel.alonso.d@4028fbad-734f-0410-9096-773eaa3a677b>
2008-07-02 21:29:03 +0000

    cambios menores

--- trunk/src/Gridfile.h
+++ trunk/src/Gridfile.h
@@ -1,21 +1,31 @@
 #ifndef GRIDFILE_H_
 #define GRIDFILE_H_
 
+#include <iostream>
+#include <vector>
+
 #include "Balde.h"
+#include "Zona.h"
+
 #define MAX_X 16
 #define MAX_Y 16
 #define MAX_Z 16
 
-class Gridfile {
-private:
-	Balde* grid[MAX_X][MAX_Y][MAX_Z];
+using namespace std;
 
-public:
-	Gridfile();
-	~Gridfile();
-	void cargarBIN();	
-	void add(short int,short int,float,int);
-	Balde* get(int,int,int);
+class Gridfile {
+	private:
+		Balde* grid[MAX_X][MAX_Y][MAX_Z];
+		vector<Zona*> mascara;
+
+	public:
+		Gridfile();
+		~Gridfile();
+		void cargarBIN();	
+		void add(short int,short int,float,int);
+		Balde* get(int,int,int);
+		void addZona(Zona*);
+		Zona* getZona(int,int,int);
 };
 
 #endif /* GRIDFILE_H_ */
