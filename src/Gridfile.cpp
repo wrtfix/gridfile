209917da27eaab88c092ab2e92496571dfdd18e2
wrtfix <wrtfix@4028fbad-734f-0410-9096-773eaa3a677b>
2008-07-03 07:44:06 +0000

    cargo las escalas medio a ojo medio enserio dentro del archivo gridfile.h y cpp

--- src/Gridfile.cpp
+++ src/Gridfile.cpp
@@ -1,11 +1,11 @@
 #include "Gridfile.h"
 
 Gridfile::Gridfile() {
-	
+
 	Balde* b = new Balde;
-	for (int i=0;i<MAX_X;i++)
-		for (int j=0;j<MAX_Y;j++)
-			for (int k=0;k<MAX_Z;k++)
+	for (int i=0;i<CAPACIDAD;i++)
+		for (int j=0;j<CAPACIDAD;j++)
+			for (int k=0;k<CAPACIDAD;k++)
 				this->grid[i][j][k] = b;	
 
 }
@@ -27,28 +27,33 @@ Balde* Gridfile::get(int x,int y,int z){
 	return this->grid[x][y][z];
 	
 	}
+
+void Gridfile::guardarEscalas(short int a[CAPACIDAD],short int f[CAPACIDAD], float p[CAPACIDAD]){
+	for (int i = 0; i<CAPACIDAD;i++)
+	{
+		this->escalaAccion[i] = a[i];
+		this->escalaForma[i] = f[i];
+		this->escalaPrecio[i] = p[i];
+	}
+}
+
 	
 //agrega resultados al grid.
 //alpha. Todavia no tiene en cuenta Zonas.
 void Gridfile::add(short int accion,short int forma,float precio,int valor){
 	
-	regBalde reg;
-	
-	
-			
 	int accion2 = (int) accion;
 	int forma2 = (int) forma;
 	int precio2 = (int) precio;
-	
+
+			cout << "Gridfile.cpp: accion2: " << accion2 << endl;
+			cout << "Gridfile.cpp: forma2: " << forma2 << endl;
+			cout << "Gridfile.cpp: precio2: " << precio2 << endl;
+
 	Balde *b = this->get(accion2, forma2, precio2);
 	
-	reg.precio = accion;
-	reg.forma = forma;
-	reg.precio = precio;
-	reg.valor = valor;
-
 	if (!b->full())
-		b->add(reg);
+		b->add(valor);
 	/*
 	* else
 	* 	DIVIDIR
