2fac71bc145a091a7118071bfe72eca2a52cb8ad
wrtfix <wrtfix@4028fbad-734f-0410-9096-773eaa3a677b>
2008-07-03 07:44:06 +0000

    cargo las escalas medio a ojo medio enserio dentro del archivo gridfile.h y cpp

--- trunk/src/Gridfile.h
+++ trunk/src/Gridfile.h
@@ -7,17 +7,17 @@
 #include "Balde.h"
 #include "Zona.h"
 
-#define MAX_X 16
-#define MAX_Y 16
-#define MAX_Z 16
+#define CAPACIDAD 16
 
 using namespace std;
 
 class Gridfile {
 	private:
-		Balde* grid[MAX_X][MAX_Y][MAX_Z];
+		Balde* grid[CAPACIDAD][CAPACIDAD][CAPACIDAD];
 		vector<Zona*> mascara;
-
+		short int escalaAccion[CAPACIDAD];
+		short int escalaForma[CAPACIDAD];
+		float escalaPrecio[CAPACIDAD];
 	public:
 		Gridfile();
 		~Gridfile();
@@ -26,6 +26,8 @@ class Gridfile {
 		Balde* get(int,int,int);
 		void addZona(Zona*);
 		Zona* getZona(int,int,int);
+		void guardarEscalas(short int a[CAPACIDAD],short int f[CAPACIDAD], float p[CAPACIDAD]);
+		
 };
 
 #endif /* GRIDFILE_H_ */
