ea965e88a465b8bd5ff99d3ccaf12ddf44b90901
wrtfix <wrtfix@4028fbad-734f-0410-9096-773eaa3a677b>
2008-07-07 10:58:28 +0000

    aplico licencia GPL a archivos de codigo.

--- trunk/src/Zona.cpp
+++ trunk/src/Zona.cpp
@@ -1,23 +1,24 @@
 /*
  *      Zona.cpp
- *      
+ *
  *      Copyright 2008 	Jorge Carlos Mendiola <wrtfix@gmail.com> &
- * 						Manuel Alonso <piratAttack@gmail.com>
- *      
- *      This program is free software; you can redistribute it and/or modify
- *      it under the terms of the GNU General Public License as published by
- *      the Free Software Foundation; either version 2 of the License, or
- *      (at your option) any later version.
- *      
- *      This program is distributed in the hope that it will be useful,
- *      but WITHOUT ANY WARRANTY; without even the implied warranty of
- *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *      GNU General Public License for more details.
- *      
- *      You should have received a copy of the GNU General Public License
- *      along with this program; if not, write to the Free Software
- *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- *      MA 02110-1301, USA.
+ * 						Manuel Alonso <manuel.alonso.d@gmail.com>
+ *
+ *
+ * Este programa es de libre distribución; puedes distribuirlo y/o modificarlo
+ * bajo los términos de la GNU General Public License como se publicó por la
+ * Free Software Foundation; bien la versión 2 de la licencia o,  bajo tu
+ * criterio, cualquier versión posterior.
+ *
+ * Este programa se distribuye con la esperanza de que sea útil, pero
+ * SIN NINGUNA GARANTÏA; sin, incluso, ninguna garantía implicada por
+ * MERCANCIBILIDAD o POR ALGÜN PROPÓSITO PARTICULAR. Lee la
+ * GNU General Public License para más información.
+ *
+ * Deberías haber recibido una copia de la GNU General Public License
+ * junto con este programa. Si no es así escribe a Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
  */
 
 #include "Zona.h"
