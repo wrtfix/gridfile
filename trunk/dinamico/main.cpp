eb63aeb4f86a7959682b2df7a65dfab17f69fccd
manzanelj <manzanelj@4028fbad-734f-0410-9096-773eaa3a677b>
2009-07-19 23:17:24 +0000

    Divide zonas

--- trunk/dinamico/main.cpp
+++ trunk/dinamico/main.cpp
@@ -18,15 +18,23 @@ int main()
     g.addxCantidad(13,4,2,350,500);
     g.addxCantidad(14,5,2,350,500);
     g.addxCantidad(15,6,2,350,200);
+    g.addxCantidad(17,13,2,350,900);
 
-    /*g.addxCantidad(20,7,7,1350,150);
-    g.addxCantidad(21,8,7,1350,50);
-    g.addxCantidad(22,9,7,1350,40);
-    g.addxCantidad(23,10,7,1350,70);
-    g.addxCantidad(24,11,7,1350,80);
-    g.addxCantidad(25,12,7,1350,45);
+    g.addxCantidad(18,19,2,350,300);
+    g.addxCantidad(19,14,2,350,325);
+    g.addxCantidad(30,15,2,350,315);
+    g.addxCantidad(31,16,2,350,316);
+    g.addxCantidad(32,17,2,350,215);
+    g.addxCantidad(33,18,2,350,150);
 
-    /*g.addxCantidad(17,13,2,350,900);
+    g.addxCantidad(20,7,7,1350,460);
+    g.addxCantidad(21,8,7,1350,450);
+    g.addxCantidad(22,9,7,1350,440);
+    g.addxCantidad(23,10,7,1350,470);
+    g.addxCantidad(24,11,7,1350,480);
+    g.addxCantidad(25,12,7,1350,145);
+
+    /*
 
 
     g.addxCantidad(26,14,2,350,300);
@@ -86,19 +94,19 @@ int main()
     cout << "Muestro el nuevo balde 1,0"<<endl;
     g.mostrar(1,0);
 
-    /*cout << "Muestro el nuevo balde 2,1"<<endl;
-    g.mostrar(2,1);
+    cout << "Muestro el nuevo balde 2,0"<<endl;
+    g.mostrar(2,0);
 
-    cout << "Muestro el nuevo balde 3,1"<<endl;
-    g.mostrar(3,1);*/
+    cout << "Muestro el nuevo balde 0,1"<<endl;
+    g.mostrar(0,1);
 
-    /*cout << "Muestro el nuevo balde 2,0"<<endl;
+    cout << "Muestro el nuevo balde 2,0"<<endl;
     g.mostrar(2,1);
 
     cout << "Muestro el nuevo balde 3,0"<<endl;
     g.mostrar(3,1);
 
-    cout << "Muestro el nuevo balde 3,1"<<endl;
+    /*cout << "Muestro el nuevo balde 3,1"<<endl;
     g.mostrar(3,1);
 
 
